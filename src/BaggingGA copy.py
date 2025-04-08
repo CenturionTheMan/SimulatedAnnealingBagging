6c933aa6fee796cad805872deb94de2106935aa1
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2025-04-08 14:38:49 +0200

    ???

--- src/BaggingGA copy.py
+++ src/BaggingGA copy.py
@@ -13,7 +13,7 @@ class BaggingGA:
     def __init__(self,
                  X: np.ndarray, y: np.ndarray,
                  n_trees: int,
-                 max_iterations: int, mutation_rate: float, crossover_rate: float, generation_gap: float, population_size: int|None = None):
+                 max_iterations: int, mutation_rate: float, crossover_rate: float, population_size: int|None = None):
         self.X = X
         self.y = y
         self.n_trees = n_trees
@@ -21,7 +21,6 @@ class BaggingGA:
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.population_size = population_size if population_size else n_trees
-        self.population_gap = generation_gap
 
     def disagreement_measure(self, models: List[BaggingModel], X_test: np.ndarray) -> List[float]:
         res = []
@@ -36,12 +35,20 @@ class BaggingGA:
     def calculate_fitness(self, models: List[BaggingModel], X_test: np.ndarray, y_test: np.ndarray) -> List[float]:
         predictions_per_model = [model.model.predict(X_test[:,model.bag.features]) for model in models]
         accuracy_per_model = [accuracy_score(y_test, pred) for pred in predictions_per_model]
-        return accuracy_per_model
+        disagreement_per_model = self.disagreement_measure(models, X_test)
+
+        alpha = 0.9
+        
+        fitness_per_model = [ 
+            accuracy * alpha + disagreement * (1 - alpha)
+            for accuracy, disagreement in zip(accuracy_per_model, disagreement_per_model)
+        ]
+        return fitness_per_model
     
-    def selection(self, population: List[Bag], fitness: List[float]) -> List[Bag]:
+    def selection(self, population: List[Bag], fitness: List[float], X_train: np.ndarray) -> List[Bag]:
         sum_fitness = sum(fitness)
         selection_probs = [f / sum_fitness for f in fitness]
-        selected_indices = np.random.choice(range(len(population)), size=int(self.population_size * (1-self.population_gap)), p=selection_probs, replace=False)
+        selected_indices = np.random.choice(range(len(population)), size=self.population_size, p=selection_probs, replace=True)
         pop = np.array(population)[selected_indices]
         return pop.tolist()        
 
@@ -55,8 +62,6 @@ class BaggingGA:
     
     def crossover(self, population: List[Bag]) -> List[Bag]:
         for i in range(0, len(population), 2):
-            if i + 1 >= len(population):
-                break
             if random.random() >= self.crossover_rate:
                 continue
             crossover_point = random.randint(0, len(population[0].X_bin) - 1)
@@ -70,8 +75,8 @@ class BaggingGA:
         best = sorted_models[:self.n_trees]
         return [model for model, _ in best]
         
-    def run(self, X_for_test=None, y_for_test=None, fun_monitor=None) -> List[BaggingModel]:
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.1, shuffle=True)
+    def run(self, X_for_test, y_for_test) -> List[BaggingModel]:
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, shuffle=True)
         
         population = create_bags(X_train, self.population_size)
         
@@ -93,19 +98,15 @@ class BaggingGA:
             if best_fitness is None or fitness_pointer > best_fitness:
                 best_fitness = fitness_pointer
                 best_models = models
+                
+            print(f"Iteration {iteration}, Best fitness: {best_fitness:.3f}, Fitness: {fitness_pointer:.3f}, Accuracy: {accuracy:.3f}")
             
-            if fun_monitor is not None:
-                fun_monitor(iteration, best_fitness, fitness_pointer, accuracy)
-            
-            population = self.selection(population, fitness)
+            population = self.selection(population, fitness, X_train)
             population = self.crossover(population)
             population = self.mutate(population)
 
-            population_gap = create_bags(X_train, int(self.population_size - (self.population_size * (1-self.population_gap))))
-            population += population_gap
-
             iteration += 1
 
-        n_best_models = self.get_n_models(best_models, best_fitness)
+        n_best_models = self.get_n_models(best_models, fitness)
         return n_best_models
     
